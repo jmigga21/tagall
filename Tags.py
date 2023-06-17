9c0081d0f18b055e0054295ef3e2aede12753b8a
jmigga21 <135898735+jmigga21@users.noreply.github.com>
2023-06-17 19:44:03 +0300

    Update Tags.py

--- Tags.py
+++ Tags.py
@@ -47,7 +47,7 @@ class TagMod(loader.Module):
         tags = []
         async for user in message.client.iter_participants(message.to_id):
             tags.append(f"<a href='tg://user?id={user.id}'>\u2060</a>")
-        chunkss = list(chunks(tags, 1))
+        chunkss = list(chunks(tags, 5))
         random.shuffle(chunkss)
         for chunk in chunkss:
             await message.client.send_message(message.to_id, tag + "\u2060".join(chunk))
